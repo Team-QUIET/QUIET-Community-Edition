69bb501aaa8f6116629bdfabf9747147b1b4eef1
Azraeel <kanyepayton1@gmail.com>
2024-09-02 12:42:49 +0900

    Introduce Proper UUID

--- mod_info.lua
+++ mod_info.lua
@@ -5,12 +5,12 @@ description = "LOUD CE is a massive collection of Bug Fixes, Game Rebalance, & O
 exclusive = false
 ui_only = false
 selectable = true
-uid = "eb6513f1-cb08-4eed-8e65-74d3f98427c2"
+uid = "cbed05f7-bcfe-4879-ad92-7a7cb2fedb15"
 
-requires = {
-	"454af309-5afb-458b-bf5b-a00000000007", --4DC
-    "25D57D85-7D84-27HT-A501-BR3WL4N000079"
-}
+--requires = {
+--	"454af309-5afb-458b-bf5b-a00000000007", --4DC
+--    "25D57D85-7D84-27HT-A501-BR3WL4N000079"
+--}
 --conflicts = {"ffffffff-9d4e-11dc-8314-0800200c0605", --Base (weak) BlackOps Adv Command Units
 --             "9e8ea941-c306-4751-b367-e00000000302" }  --Base (weak) Enhanced Commanders
 
