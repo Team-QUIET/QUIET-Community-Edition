4d03b9f3adc18092e096b5a4eab5985b3680a0ba
Azraeel <45247351+Azraeel@users.noreply.github.com>
2024-09-15 20:16:49 +0900

    Update mod_info.lua

--- mod_info.lua
+++ mod_info.lua
@@ -1,6 +1,6 @@
 name = "LOUD Community Edition"
 author = "LOUD CE Team"
-version = 1.26
+version = 1.27
 description = "LOUD CE is a massive collection of Bug Fixes, Game Rebalance, & Other Various Actions"
 exclusive = false
 ui_only = false
