9135a1d8c9ae6f4d5a83ad98026dd5814227af78
Azraeel <45247351+Azraeel@users.noreply.github.com>
2024-09-22 22:03:15 +0900

    Introduce LCE E&F Conflicts & Requirements

--- modules/LCE Economy & Flow/mod_info.lua
+++ modules/LCE Economy & Flow/mod_info.lua
@@ -6,8 +6,11 @@ exclusive = false
 ui_only = false
 selectable = true
 uid = "zzzzzzze-fffe-fffe-fffe-94ccddf9599f"
---requires = {
---	"454af309-5afb-458b-bf5b-a00000000007", --4DC
---  "25D57D85-7D84-27HT-A501-BR3WL4N000079"
---  "fffffffe-6e98-4864-9599-4133236eea7a"
---}
+requires = {
+  "zzzzzzze-fffe-fffe-fffe-71aabbf8363f", --LCE
+}
+conflicts = { 
+  "ffffffff-6f00-4864-9599-4133236eea7a", --LOUD Evenflow Engineers
+}
+before = { }
+after = { }
