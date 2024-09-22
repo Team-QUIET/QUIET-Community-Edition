fcfa4ed6546e17c879b6393f66bae6163004e42c
Azraeel <45247351+Azraeel@users.noreply.github.com>
2024-09-22 22:05:55 +0900

    Introduce LCE Conflicts

--- mod_info.lua
+++ mod_info.lua
@@ -6,8 +6,9 @@ exclusive = false
 ui_only = false
 selectable = true
 uid = "zzzzzzze-fffe-fffe-fffe-71aabbf8363f"
---requires = {
---	"454af309-5afb-458b-bf5b-a00000000007", --4DC
---  "25D57D85-7D84-27HT-A501-BR3WL4N000079"
---  "fffffffe-6e98-4864-9599-4133236eea7a"
---}
+requires = { }
+conflicts = { 
+  "ffffffff-9d4e-11dc-8314-0800200c0605", --LOUD Enhanced Commanders
+}
+before = { }
+after = { }
