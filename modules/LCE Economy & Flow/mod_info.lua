d62ee86cf4043db5c0e912d1e6eb99782beba9d7
Azraeel <45247351+Azraeel@users.noreply.github.com>
2024-09-24 16:44:48 +0900

    Update mod_info.lua

--- modules/LCE Economy & Flow/mod_info.lua
+++ modules/LCE Economy & Flow/mod_info.lua
@@ -1,13 +1,13 @@
-name = "LOUD Community Edition - Economy & Flow"
-author = "LOUD CE Team"
+name = "QUIET - Economy & Flow"
+author = "QUIET Team"
 version = 1.1
-description = "LCE E&F is the economic overhaul part of Loud Community Edition. This changes factories, mass extractors, & introduces a custom evenflow."
+description = "QUIET E&F is the economic overhaul part of QUIET. This changes factories, mass extractors, & introduces a custom evenflow."
 exclusive = false
 ui_only = false
 selectable = true
 uid = "zzzzzzze-fffe-fffe-fffe-94ccddf9599f"
 requires = {
-  "zzzzzzze-fffe-fffe-fffe-71aabbf8363f", --LCE
+  "zzzzzzze-fffe-fffe-fffe-71aabbf8363f", --QUIET
 }
 conflicts = { 
   "ffffffff-6f00-4864-9599-4133236eea7a", --LOUD Evenflow Engineers
