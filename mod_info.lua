fb3b72c310e0fc13a6b0648e3509a21c1272cc4a
Azraeel <45247351+Azraeel@users.noreply.github.com>
2024-09-25 00:41:57 +0900

    Update mod_info.lua

--- mod_info.lua
+++ mod_info.lua
@@ -1,6 +1,6 @@
 name = "QUIET"
 author = "QUIET Team"
-version = 1.31
+version = 1.32
 description = "QUIET is a massive collection of Bug Fixes, Game Rebalance, & Other Various Actions"
 exclusive = false
 ui_only = false
