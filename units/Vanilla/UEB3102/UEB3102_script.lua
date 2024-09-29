52b4725362d55ffe97f8e6b6a293d2015ed4a50f
rajderks <2419711+rajderks@users.noreply.github.com>
2024-09-29 21:21:54 +0200

    UEB3102 new scm and targetBones

---
+++ units/Vanilla/UEB3102/UEB3102_script.lua
@@ -0,0 +1,9 @@
+local TSonarUnit = import('/lua/defaultunits.lua').SonarUnit
+
+UEB3102 = Class(TSonarUnit) {
+    TimedSonarTTIdleEffects = {
+        { Bones = {'B14'}, Offset = {0,-0.6,0}, Type = 'SonarBuoy01' },
+    },
+}
+
+TypeClass = UEB3102
