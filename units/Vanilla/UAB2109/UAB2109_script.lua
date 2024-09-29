8858433796577f9a82bdb674a2818da8f03a7264
rajderks <2419711+rajderks@users.noreply.github.com>
2024-09-29 21:08:17 +0200

    UAB2109 few scm and targetbones

---
+++ units/Vanilla/UAB2109/UAB2109_script.lua
@@ -0,0 +1,25 @@
+local AStructureUnit = import('/lua/defaultunits.lua').StructureUnit
+
+local AANChronoTorpedoWeapon = import('/lua/aeonweapons.lua').AANChronoTorpedoWeapon
+
+UAB2109 = Class(AStructureUnit) {
+
+    Weapons = {
+        Turret01 = Class(AANChronoTorpedoWeapon) {},
+    },
+	
+	OnCreate = function(self)
+		AStructureUnit.OnCreate(self)
+
+        self.DomeEntity = import('/lua/sim/Entity.lua').Entity({Owner = self,})
+        self.DomeEntity:AttachBoneTo( -1, self, 'UAB2109' )
+        self.DomeEntity:SetMesh('/effects/Entities/UAB2109_Dome/UAB2109_Dome_mesh')
+        self.DomeEntity:SetDrawScale(0.3)
+        self.DomeEntity:SetVizToAllies('Intel')
+        self.DomeEntity:SetVizToNeutrals('Intel')
+        self.DomeEntity:SetVizToEnemies('Intel')         
+        self.Trash:Add(self.DomeEntity)
+	end,       
+}
+
+TypeClass = UAB2109
