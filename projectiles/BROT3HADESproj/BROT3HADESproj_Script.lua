0cf75ba57c892f80bd687e557ee32c3762da4dc4
Azraeel <45247351+Azraeel@users.noreply.github.com>
2024-09-29 18:07:05 +0900

    Introduce T4 Avernus  (#205)
    * Introduce the T4 Aeon Avernus
    
    * Adjust the Avernus
    
    * T4 Avernus Additional Cost Reduction

---
+++ projectiles/BROT3HADESproj/BROT3HADESproj_Script.lua
@@ -0,0 +1,13 @@
+-- ****************************************************************************
+-- **
+-- **  File     : 
+-- **  Author(s):
+-- **
+-- **  Summary  :
+-- **
+-- **  Copyright � 2007 Gas Powered Games, Inc.  All rights reserved.
+-- ****************************************************************************
+local AeonBROT3HADESproj = import('/lua/aeonprojectiles.lua').AeonBROT3HADESproj
+BROT3HADESproj = Class(AeonBROT3HADESproj) {}
+
+TypeClass = BROT3HADESproj
