8450001042ea552f22fa7e4b19e2b61f080b39fc
rajderks <2419711+rajderks@users.noreply.github.com>
2024-09-30 08:02:39 +0200

    V1.38 - Release point

--- mod_info.lua
+++ mod_info.lua
@@ -1,14 +1,14 @@
 name = "QUIET"
 author = "QUIET Team"
-version = 1.37
+version = 1.38
 description = "QUIET is a massive collection of Bug Fixes, Game Rebalance, & Other Various Actions"
 exclusive = false
 ui_only = false
 selectable = true
 uid = "zzzzzzze-fffe-fffe-fffe-71aabbf8363f"
-requires = { }
-conflicts = { 
-  "ffffffff-9d4e-11dc-8314-0800200c0605", --LOUD Enhanced Commanders
+requires = {}
+conflicts = {
+	"ffffffff-9d4e-11dc-8314-0800200c0605", --LOUD Enhanced Commanders
 }
-before = { }
-after = { }
+before = {}
+after = {}
