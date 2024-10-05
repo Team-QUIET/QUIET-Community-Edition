f27660e4221e1f7e3c0e2d30f9d0aed93e3f2c83
Azraeel <kanyepayton1@gmail.com>
2024-10-05 19:40:15 +0900

    Nerf Banisher

---
+++ projectiles/WBPSmallAAMissile03/WBPSmallAAMissile03_script.lua
@@ -0,0 +1,9 @@
+local TMissileAAProjectile = import('/lua/terranprojectiles.lua').TMissileAAProjectile
+
+StarAdderMissilesAir = import('/mods/BattlePack/lua/BattlePackprojectiles.lua').StarAdderMissilesAir
+
+WBPSmallAAMissile03 = Class(StarAdderMissilesAir) {
+}
+
+TypeClass = WBPSmallAAMissile03
+
