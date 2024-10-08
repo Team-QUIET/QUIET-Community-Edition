2530d0c086289c60705e5138e491eb472816dbc9
Azraeel <kanyepayton1@gmail.com>
2024-10-08 14:22:35 +0900

    Add Required Mods

--- mod_info.lua
+++ mod_info.lua
@@ -6,9 +6,19 @@ exclusive = false
 ui_only = false
 selectable = true
 uid = "zzzzzzze-fffe-fffe-fffe-71aabbf8363f"
-requires = {}
+requires = {
+	"454af309-5afb-458b-bf5b-a00000000007", -- 4DC
+	"9e8ea941-c306-4751-b367-a11000000502", -- BlackOpsUnleashed
+	"9e8ea941-c306-4751-b367-e00000000302", -- BlackOpsACUs
+	"25D57D85-7D84-27HT-A501-BR3WL4N000079",-- BrewLAN
+	"62e2j64a-53a2-y6sg-32h5-146as555a18u3",-- Total Mayhem
+	"9a9C61C0-1787-10DF-A0AD-BATTLEPACK002",-- WyvernBattlePack
+	"fffffffe-6e98-4864-9599-4133236eea7a", -- LOUD Unit Additions
+	"ffffffff-6e98-4864-9599-4133236eea7a", -- LOUD Integrated Storage
+	"ffffffff-ffff-ffff-ffff-fffffffffffe", -- LOUD Structure Enhancements
+}
 conflicts = {
-	"ffffffff-9d4e-11dc-8314-0800200c0605", --LOUD Enhanced Commanders
+	"ffffffff-9d4e-11dc-8314-0800200c0605", -- LOUD Enhanced Commanders
 }
 before = {}
 after = {}
