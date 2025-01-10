cc80a0c3c74c36615bba85d31f56ced2ab9b0a4a
loudpatches <loudpatches@that-place>
2025-01-10 09:30:45 -0600

    version bump 1.86

--- mod_info.lua
+++ mod_info.lua
@@ -1,6 +1,6 @@
 name = "QUIET"
 author = "QUIET Team"
-version = 1.85
+version = 1.86
 description = "QUIET is a massive collection of Bug Fixes, Game Rebalance, & Other Various Actions"
 exclusive = false
 ui_only = false
