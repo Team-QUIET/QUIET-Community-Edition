65c93d437a6198538ce00dda4b2254c384a0c602
Azraeelian Angel <kanyepayton1@gmail.com>
2025-02-17 08:11:17 +1000

    V1.93 Release Point + Changelog

--- mod_info.lua
+++ mod_info.lua
@@ -1,6 +1,6 @@
 name = "QUIET"
 author = "QUIET Team"
-version = 1.92
+version = 1.93
 description = "QUIET is a massive collection of Bug Fixes, Game Rebalance, & Other Various Actions"
 exclusive = false
 ui_only = false
