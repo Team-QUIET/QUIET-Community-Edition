e849e62e123a42802df929073badd82ce8d16c71
Azraeel <kanyepayton1@gmail.com>
2025-05-02 00:52:31 +0900

    T2 Aeon Destroyer

---
+++ hook/projectiles/AANDepthCharge01/AANDepthCharge01_script.lua
@@ -0,0 +1,5 @@
+local ADepthChargeProjectile = import('/lua/aeonprojectiles.lua').ADepthChargeProjectile
+
+AANDepthCharge01 = ClassProjectile(ADepthChargeProjectile) {}
+
+TypeClass = AANDepthCharge01
