9aa13a6683e6e5cadc8005d4ecae489e527f57f1
Azraeel <kanyepayton1@gmail.com>
2025-05-03 14:59:52 +0900

    Few Tweaks to Cybran Torpedo Bomber

---
+++ hook/projectiles/AANTorpedoCluster01/AANTorpedoCluster01_script.lua
@@ -0,0 +1,5 @@
+local ATorpedoSplitProjectile = import('/lua/aeonprojectiles.lua').ATorpedoSplitProjectile
+
+AANTorpedoCluster01 = ClassProjectile(ATorpedoSplitProjectile) {}
+
+TypeClass = AANTorpedoCluster01
