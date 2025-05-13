be4f460f0e0ee07a2c2f5e8e649b5625df3a54e6
Azraeel <kanyepayton1@gmail.com>
2025-05-13 21:41:56 +0900

    V2.18 Release Point

--- mod_info.lua
+++ mod_info.lua
@@ -1,6 +1,6 @@
 name = "QUIET"
 author = "QUIET Team"
-version = 2.17
+version = 2.18
 description = "QUIET is a massive collection of Bug Fixes, Game Rebalance, & Other Various Actions"
 exclusive = false
 ui_only = false
