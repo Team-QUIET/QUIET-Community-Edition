9f14d0ca13314335d183a74b27078b244a3c62e6
Azraeel <kanyepayton1@gmail.com>
2025-07-13 13:15:59 +0900

    V2.25 Version

--- mod_info.lua
+++ mod_info.lua
@@ -1,6 +1,6 @@
 name = "QUIET"
 author = "QUIET Team"
-version = 2.24
+version = 2.25
 description = "QUIET is a massive collection of Bug Fixes, Game Rebalance, & Other Various Actions"
 exclusive = false
 ui_only = false
