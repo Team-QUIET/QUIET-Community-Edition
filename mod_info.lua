616bd05e2f53c4b371bf13b4c63ecf44771256e1
AzraeelianAngel <kanyepayton1@gmail.com>
2026-01-03 09:39:00 +0900

    V2.51 Release Point

--- mod_info.lua
+++ mod_info.lua
@@ -1,6 +1,6 @@
 name = "QUIET"
 author = "QUIET Team"
-version = 2.50
+version = 2.51
 description = "QUIET is a massive collection of Bug Fixes, Game Rebalance, & Other Various Actions"
 exclusive = false
 ui_only = false
@@ -11,9 +11,9 @@ requires = {
 	"454af309-5afb-458b-bf5b-a00000000007", -- 4DC
 	"9e8ea941-c306-4751-b367-a11000000502", -- BlackOpsUnleashed
 	"9e8ea941-c306-4751-b367-e00000000302", -- BlackOpsACUs
-	"25D57D85-7D84-27HT-A501-BR3WL4N000079",-- BrewLAN
-	"62e2j64a-53a2-y6sg-32h5-146as555a18u3",-- Total Mayhem
-	"9a9C61C0-1787-10DF-A0AD-BATTLEPACK002",-- WyvernBattlePack
+	"25D57D85-7D84-27HT-A501-BR3WL4N000079", -- BrewLAN
+	"62e2j64a-53a2-y6sg-32h5-146as555a18u3", -- Total Mayhem
+	"9a9C61C0-1787-10DF-A0AD-BATTLEPACK002", -- WyvernBattlePack
 	"fffffffe-6e98-4864-9599-4133236eea7a", -- LOUD Unit Additions
 	"ffffffff-6e98-4864-9599-4133236eea7a", -- LOUD Integrated Storage
 	"ffffffff-ffff-ffff-ffff-fffffffffffe", -- LOUD Structure Enhancements
